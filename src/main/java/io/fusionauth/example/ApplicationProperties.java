4a54c6d6018eb67b14bb455b55c1ae6246a5b2dd
Dan Moore <github@mooreds.com>
2020-05-07 13:43:35 -0600

    Broke out url and api key to properties file. Formatted all java files.

---
+++ src/main/java/io/fusionauth/example/ApplicationProperties.java
@@ -0,0 +1,34 @@
+package io.fusionauth.example;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ApplicationProperties {
+
+	private static String apiKey;
+	private static String fusionAuthURL;
+
+	public static String getApiKey() {
+		return apiKey;
+	}
+
+	public static String getFusionAuthURL() {
+		return fusionAuthURL;
+	}
+
+	public static void setupProperties() {
+		try (InputStream input = ApplicationProperties.class.getResourceAsStream("/application.properties")) {
+
+			Properties prop = new Properties();
+
+			// load a properties file
+			prop.load(input);
+			fusionAuthURL = prop.getProperty("fusionAuthURL");
+			apiKey = prop.getProperty("apiKey");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
