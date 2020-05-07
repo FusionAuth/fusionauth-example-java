af02a18eb3df6334cd43b370d171b98a49089521
Dan Moore <github@mooreds.com>
2020-05-07 13:43:30 -0600

    Broke out url and api key to properties file. Formatted all java files.

--- src/main/java/io/fusionauth/example/ReactivateUser.java
+++ src/main/java/io/fusionauth/example/ReactivateUser.java
@@ -1,4 +1,5 @@
 package io.fusionauth.example;
+
 import java.util.UUID;
 import com.inversoft.error.Errors;
 import io.fusionauth.client.FusionAuthClient;
@@ -8,26 +9,27 @@ import com.inversoft.rest.ClientResponse;
 
 public class ReactivateUser {
 
-    public static void main(String[] args) {    
-        
+	public static void main(String[] args) {
+
 		ApplicationProperties.setupProperties();
-           	 
-    	//Initiating the client
-    	FusionAuthClient client = new FusionAuthClient(ApplicationProperties.getApiKey(), ApplicationProperties.getFusionAuthURL());    
-        
-        UUID userId = UUID.fromString("c7f91df7-ed89-410b-87e7-a2b7ade9bf98"); 
-        
-        //Using the returned ClientResponse object
-        ClientResponse<UserResponse, Errors> response = client.reactivateUser(userId);        
-           
-        if (response.wasSuccessful()) {
-        //Outputting the user's profile details            
-            System.out.println(response.successResponse.user);             
-            
-        } else {
-            //Handling errors
-            System.out.println(response.errorResponse);
-        }
-    }
+
+		// Initiating the client
+		FusionAuthClient client = new FusionAuthClient(ApplicationProperties.getApiKey(),
+				ApplicationProperties.getFusionAuthURL());
+
+		UUID userId = UUID.fromString("c7f91df7-ed89-410b-87e7-a2b7ade9bf98");
+
+		// Using the returned ClientResponse object
+		ClientResponse<UserResponse, Errors> response = client.reactivateUser(userId);
+
+		if (response.wasSuccessful()) {
+			// Outputting the user's profile details
+			System.out.println(response.successResponse.user);
+
+		} else {
+			// Handling errors
+			System.out.println(response.errorResponse);
+		}
+	}
 
 }
